05328baf1cc865082917fe9156ef3e0e7550dd81
Michael Salerno <msalerno1993@gmail.com>
2022-03-06 05:11:19 -0500

    imported packages to index.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import React from "react";
 
 class App extends React.Component {
 
-  componentDidMount() {
-    fetch('http://[::1]:3000/api/v1/workouts/1', {
-      method: 'GET'
-  })
-    .then(response => response.json())
-    .then(resp => console.log(resp))
-  }
-
   render(){
     return (
       <div>
