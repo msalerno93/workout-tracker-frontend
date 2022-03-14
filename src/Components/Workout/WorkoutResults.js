4c0fdf1362f3886b6c99ef23e830f89622cf06f7
Michael Salerno <msalerno1993@gmail.com>
2022-03-14 00:43:05 -0400

    figured issue with exercises not displaying

--- src/Components/Workout/WorkoutResults.js
+++ src/Components/Workout/WorkoutResults.js
@@ -7,7 +7,16 @@ function WorkoutResults({workout}) {
                return <div key={w.id}>
                    <h3>{w.title}</h3>
                    <h5>{w.date}</h5>
-                   <h5>{w.workout_note}</h5>
+                   <h5>{w.exercises.map((e) =>{
+                       return <div key={e.id}>
+                           <h3>{e.name}</h3>
+                           <h3>{e.muscle_group}</h3>
+                           <h3>{e.weight}</h3>
+                           <h3>{e.reps}</h3>
+                           <h3>{e.sets}</h3>
+                           <h3>{e.exercise_note}</h3>
+                       </div>
+                   })}</h5>
                </div>
            })}
        </div>
